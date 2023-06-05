cc96b11253ffef8db436803683305aac9981919a
ChaitanyaB21 <113889111+ChaitanyaB21@users.noreply.github.com>
2023-06-05 11:29:45 +0530

    Update PencilBtn.js

--- src/Components/PencilBtn.js
+++ src/Components/PencilBtn.js
@@ -42,7 +42,7 @@ const PencilBtn = (props) => {
         <React.Fragment>
             {!showForm ? (
                 <button onClick={handleClick} id={props.id} type="button" className="cbbtn">
-                    <i className="fa fa-pencil" aria-hidden="true"></i>
+                    <i className="fa fa-pencil" ></i>
                 </button>
             ) : (
                 <form onSubmit={handleChangeformSubmit} >
